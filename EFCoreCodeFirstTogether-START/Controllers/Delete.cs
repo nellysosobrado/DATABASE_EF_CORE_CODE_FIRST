125848b847d7436648d0c248bcc8b6464ddafae7
Nelly <Nelly.sosobrado@gmail.com>
2024-12-14 14:14:29 +0100

    SRP, into new classes Detel, read, update

---
+++ EFCoreCodeFirstTogether-START/Controllers/Delete.cs
@@ -0,0 +1,39 @@
+﻿using EFCoreCodeFirstTogether_START.Data;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstTogether_START.Controllers
+{
+    public class Delete
+    {
+        public RunDelete()
+        {
+            // ==================================================================
+            // 19: DELETE DELETE DELETE DELETE DELETE DELETE DELETE DELETE DELETE
+            using (var dbContext = new ApplicationDbContext(options.Options))
+            {
+                Console.WriteLine("(D)ELETE en befintlig person");
+                Console.WriteLine("=====================");
+
+                // Vilken person ska raderas?
+                foreach (var person in dbContext.Person)
+                {
+                    Console.WriteLine($"Id: {person.Id}");
+                    Console.WriteLine($"Namn: {person.Name}");
+                    Console.WriteLine("====================");
+                }
+
+                Console.WriteLine("Välj Id på den Person som du vill radera");
+                var personIdToDelete = Convert.ToInt32(Console.ReadLine());
+                var personToDelete = dbContext.Person.First(p => p.Id == personIdToDelete);
+                dbContext.Person.Remove(personToDelete);
+
+                dbContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ EFCoreCodeFirstTogether-START/Controllers/Read.cs
@@ -0,0 +1,45 @@
+﻿using EFCoreCodeFirstTogether_START.Data;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreCodeFirstTogether_START.Controllers
+{
+    public class Read
+    {
+        public void RunRead()
+        {
+            // =====================================================================
+            // READ READ READ READ READ READ READ READ READ READ READ READ READ READ
+            using (var dbContext = new ApplicationDbContext(options.Options))
+            {
+                foreach (var person in dbContext.Person)
+                {
+                    Console.WriteLine($"Namn: {person.Name}");
+                    Console.WriteLine($"Ålder: {person.Age}");
+                    Console.WriteLine("====================");
+                }
+
+                // Hmmm varför ser inte jag någon information i 'County' ?
+                // 17b: Jag måste nog köra INCLUDE för att få se datat....
+
+                //foreach (var person in dbContext.Person.Include(c => c.County))
+                //{
+                //    Console.WriteLine($"Namn: {person.Name}");
+                //    Console.WriteLine($"Ålder: {person.Age}");
+
+                //    if (person.County != null)
+                //    {
+                //        Console.WriteLine($"County kontakperson: {person.County.ContactPerson}");
+                //    }
+
+                //    Console.WriteLine("====================");
+                //}
+            }
+
+        }
+    }
+}
